143e8aefbbe74a9e77e72adee4f7119aa3640d57
ChickRicks <ricardolucasb@gmail.com>
2026-09-04 19:07:37 -0300

    Adicionado Teste 3

---
+++ Testes/Testes certos/Teste3(Tokens válidos).lua
@@ -0,0 +1,20 @@
+if (1 > 2) then
+    while (1 > 2) do
+        for i = 1, 10, 1 do
+            print("teste")
+        end
+    end
+end
+function Teste(...)
+    local x = 1
+    x = x + 1
+    x = x - 1
+    if (x >= x) then
+        if (x <= x) then
+            if (x ~= x) then
+                local texto = "texto"
+            end
+        end
+    end
+    return x
+end
